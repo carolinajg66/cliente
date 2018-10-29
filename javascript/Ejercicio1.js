d1a96a542d8a70eedf6e40c2991400b077e3775b
Ana Carolina <carolinajustinianoguzman@gmail.com>
2018-10-29 18:34:47 +0100

    numero aleario hecho

--- javascript/Ejercicio1.js
+++ javascript/Ejercicio1.js
@@ -1,6 +1,21 @@
-var numeroAletorio = Math.round(Math.random() * 100);
-            
-document.write(numeroAletorio);
+function NumeroAletario() {
+
+    var numerosAleatorios = [];
+    var text = "";
+
+    for (var i=0; i < 100; i++) {
+        numerosAleatorios.push(Math.round(Math.random() * 100));
+        text += numerosAleatorios[i] + "<br>";
+        document.getElementById("demo").innerHTML = text;
+    }
+    
+}
+
+function ordenar(){
+
+
+}
+NumeroAletario();
 
 
 
