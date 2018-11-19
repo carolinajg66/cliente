182944aec887fe24bde8b651c0881ca98a947551
Ana Carolina <carolinajustinianoguzman@gmail.com>
2018-11-19 19:01:53 +0100

    hjola

--- javascript/Ejercicio5.js
+++ javascript/Ejercicio5.js
@@ -7,29 +7,28 @@ $(document).ready(function(){
   
     for (var i=0; boton.length ; i++){ 
    
-        //alert("hola");
         boton[i].addEventListener("click", aumentar, false);
-        //boton[i].addEventListener("click", disminuir, false);
+        boton[i].addEventListener("click", disminuir, false);
     }
 
         function aumentar(e){
             var sitio; 
             //alert(e.target.id);
           
-            if(e.target==aumento1){
+            if(e.target==id_aumento1){
                 sitio="#textoUno";
 
-            }else if(e.target==aumento2){
+            }else if(e.target==id_aumento2){
                 sitio="#textoDos";
                 
-            }else if(e.target==aumento3){
+            }else if(e.target==id_aumento3){
                 sitio="#textoTres";
             }
             var porDefecto = $(sitio).css("font-size");
 
             porDefecto=parseInt(porDefecto);
 
-            $(sitio).css("font-size", porDefecto*1.1);*/
+            $(sitio).css("font-size", porDefecto*1.1);
         }
 
 
@@ -51,6 +50,6 @@ $(document).ready(function(){
 
             $(sitio).css("font-size", porDefecto*1.1);
         }
-*/
+
        
   });
