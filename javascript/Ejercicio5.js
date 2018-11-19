094043f0d81107c996dd0be5aa642b8a8c2e0f0b
Ana Carolina <carolinajustinianoguzman@gmail.com>
2018-11-19 17:27:33 +0100

    hola3

--- javascript/Ejercicio5.js
+++ javascript/Ejercicio5.js
@@ -2,17 +2,19 @@ $(document).ready(function(){
 
 
     var boton=document.querySelectorAll("button");
+    
 
   
-    for (var i=0; boton.lengh; i++){
+    for (var i=0; i<boton.lengh; i++){
         
         boton[i].addEventListener("click", aumentar, false);
-        boton[i].addEventListener("click", disminuir, false);
-
+        //boton[i].addEventListener("click", disminuir, false);
+    }
 
         function aumentar(e){
-            var sitio; 
-          
+            //var sitio; 
+            alert(e.target.id);
+          /*
             if(e.target==aumento1){
                 sitio="#textoUno";
 
@@ -22,12 +24,32 @@ $(document).ready(function(){
             }else if(e.target==aumento3){
                 sitio="#textoTres";
             }
+            var porDefecto = $(sitio).css("font-size");
+
+            porDefecto=parseInt(porDefecto);
+
+            $(sitio).css("font-size", porDefecto*1.1);*/
         }
 
-        $(sitio).css
+/*
+        function disminuir(e){
+            var lugar; 
+          
+            if(e.target==disminuir1){
+                lugar="#textoUno";
 
-    }
+            }else if(e.target==disminuir2){
+                lugar="#textoDos";
+                
+            }else if(e.target==disminuir3){
+                lugar="#textoTres";
+            }
+            var porDefecto = $(lugar).css("font-size");
 
+            porDefecto=parseInt(porDefecto);
 
-
+            $(sitio).css("font-size", porDefecto*1.1);
+        }
+*/
+       
   });
