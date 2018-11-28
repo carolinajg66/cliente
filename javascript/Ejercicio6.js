851be81861840612d9b68d60f348666bcf7b001e
Ana Carolina <carolinajustinianoguzman@gmail.com>
2018-11-28 21:49:30 +0100

    a

---
+++ javascript/Ejercicio6.js
@@ -0,0 +1,93 @@
+/*Galeria de imagenes. cambia una imagen en blanco y negro por otra en color. Y, un filtro por botones */
+
+document.querySelectorAll("img");
+
+for(var i=0; i<imagenes.lengt; i++){
+    imagenes[i]=document.addEvenListener("mouseover", active, false);
+     imagenes[i]=document.addEvenListener("mouseout", desactive, false);
+
+}
+
+function activa(e){
+
+    if(e.target==img1){
+       $("#img1").attr("src","img/img1.png" );
+    }
+   if(e.target==img2){
+       $("#img2").attr("src","img/img2.png" );
+    }
+    if(e.target==img3){
+       $("#img3").attr("src","img/img3.png" );
+    }
+    if(e.target==img4){
+       $("#img4").attr("src","img/img4.png" );
+    }
+    if(e.target==img4){
+        $("#img4").attr("src","img/img4.png" );
+    }
+    if(e.target==img4){
+        $("#img4").attr("src","img/img4.png" );
+    }
+    if(e.target==img4){
+        $("#img4").attr("src","img/img4.png" );
+    }
+    if(e.target==img4){
+        $("#img4").attr("src","img/img4.png" );
+    }
+    if(e.target==img4){
+        $("#img4").attr("src","img/img4.png" );
+    }
+    if(e.target==img4){
+        $("#img4").attr("src","img/img4.png" );
+    }
+    if(e.target==img4){
+        $("#img4").attr("src","img/img4.png" );
+    }
+    if(e.target==img4){
+        $("#img4").attr("src","img/img4.png" );
+    }
+    if(e.target==img4){
+        $("#img4").attr("src","img/img4.png" );
+    }
+    if(e.target==img4){
+        $("#img4").attr("src","img/img4.png" );
+    }
+    if(e.target==img4){
+        $("#img4").attr("src","img/img4.png" );
+    }
+    if(e.target==img4){
+        $("#img4").attr("src","img/img4.png" );
+    }
+    if(e.target==img4){
+        $("#img4").attr("src","img/img4.png" );
+    }
+    if(e.target==img4){
+        $("#img4").attr("src","img/img4.png" );
+    }
+    if(e.target==img4){
+        $("#img4").attr("src","img/img4.png" );
+    }
+    if(e.target==img4){
+        $("#img4").attr("src","img/img4.png" );
+    }
+
+     
+}
+
+function desactiva(){
+    if(e.target==img1){
+        $("#img1").attr("src","img/img1.png" );
+    }
+   if(e.target==img2){
+        $("#img1").attr("src","img/img2.png" );
+    }
+    if(e.target==img3){
+        $("#img1").attr("src","img/img3.png" );
+    }
+    if(e.target==img4){
+        $("#img1").attr("src","img/img4.png" );
+    }
+ $("#img1").attr("src","img/img2.png" );
+ }
+
+}
