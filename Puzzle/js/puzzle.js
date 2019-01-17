9518cb1037f9e8572756d08b3c05742177dcdc15
Ana Carolina <carolinajustinianoguzman@gmail.com>
2019-01-17 20:20:21 +0100

    holaaa

---
+++ Puzzle/js/puzzle.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+
+
+    texto.addEventListener("click", mostrar, false); 
+    
+
+});
+
+function genera_tabla() {
+    // Obtener la referencia del elemento body
+    //var body = document.getElementsByTagName("body")[0];
+    var section=document.getElementsByTagName("section")[0];
+   
+    // Crea un elemento <table> y un elemento <tbody>
+    var tabla   = document.createElement("table");
+    var tblBody = document.createElement("tbody");
+   
+    // Crea las celdas
+    for (var i = 0; i < 4; i++) {
+      // Crea las hileras de la tabla
+      var hilera = document.createElement("tr");
+   
+      for (var j = 0; j < 4; j++) {
+        // Crea un elemento <td> y un nodo de texto, haz que el nodo de
+        // texto sea el contenido de <td>, ubica el elemento <td> al final
+        // de la hilera de la tabla
+        var celda = document.createElement("td");
+        var textoCelda = document.createTextNode("celda en la hilera "+i+", columna "+j);
+        celda.appendChild(textoCelda);
+        hilera.appendChild(celda);
+      }
+   
+      // agrega la hilera al final de la tabla (al final del elemento tblbody)
+      tblBody.appendChild(hilera);
+    }
+   
+    // posiciona el <tbody> debajo del elemento <table>
+    tabla.appendChild(tblBody);
+    // appends <table> into <body>
+    section.appendChild(tabla);
+    //body.appendChild(tabla);
+    // modifica el atributo "border" de la tabla y lo fija a "2";
+    tabla.setAttribute("border", "2");
+  }
--- javascript/Ejercicio10.js
+++ javascript/Ejercicio10.js
@@ -1,9 +1,45 @@
 /*PARTE DOS EJERCICIO CARRITO*/
 $(function(){
+    //menu desplegable con Jquery  
 
     $( ).
      
     http://jsfiddle.net/ZyUYN/
+    //.val para coger infomacion de los input
+    //En el boton ponemos type=submmit 
+
+    $("#form_DATOS").submit(function(){
+
+        var miNombre=$("#nombre").val();
+        alert("mi nombre es"+miNombre);
+
+        $(":text").each (function(){//parac cada campo de tipo text
+            alert($(this).val());
+
+        });
+
+    });
+
+    $("#form_DATOS").submit(function(){
+
+       if( $("#nombre").val==""){
+           alert("rellena el campo del nombre ");
+           $("#nombre").focus();
+           return false; 
+       }
+
+    
+
+    });
+    $("#edad").blur(function(){
+
+           var miEdad=$(this).val(); 
+           if(isNam(miEdad)){
+               alert("Escribe tu edad en numeros");
+               $(this).focus();
+               $(this).val("");
+           }
+    });
 
     //pinetools 
 
